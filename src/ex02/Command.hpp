33694fed479039eaffce870eba8d27bff68e8998
Noxe42 <amineouahab63@gmail.com>
2024-09-20 22:16:15 +0200

    gestion de ping

--- src/ex02/Command.hpp
+++ src/ex02/Command.hpp
@@ -1,19 +1,14 @@
-// Command.hpp
-
 #ifndef COMMAND_HPP
-#define COMMAND_HPP
+# define COMMAND_HPP
 
-#include "Client.hpp"
-#include <vector>
-#include <string>
+# include "Client.hpp"
+# include <vector>
+# include <string>
 
 class Command {
-public:
-    virtual ~Command() {}
-    virtual void execute(int clientFd, std::map<int, Client*>& clients, const std::vector<std::string>& args) = 0;
+    public:
+        virtual ~Command() {}
+        virtual void execute(int clientFd, std::map<int, Client*>& clients, const std::vector<std::string>& args) = 0;
 };
-
-void    sendPeriodicPong(int clientFd, std::map<int, Client*>& clients);
-void    startPongService(int clientFd, std::map<int, Client*>& clients);
 
 #endif
--- src/ex02/CommandHandler.cpp
+++ src/ex02/CommandHandler.cpp
@@ -1,6 +1,7 @@
 #include "CommandHandler.hpp"
 #include "Command.hpp"
 #include "PassCommand.hpp"
+#include <ctime>
 #include "PingCommand.hpp"
 #include "NickCommand.hpp"
 #include "UserCommand.hpp"
@@ -31,24 +32,41 @@ void CommandHandler::registerCommand(const std::string& name, Command* command) 
 
 Command* CommandHandler::createCommand(const std::string& name) {
     // Vérifier si la commande existe dans _commands
-    if (_commands.find(name) != _commands.end()) {
+    if (_commands.find(name) != _commands.end())
         return _commands[name];
-    }
     return NULL;
 }
 
 void CommandHandler::handleCommand(int clientFd, const std::vector<std::string>& args) {
+    // Vérifier si la commande est présente dans les arguments
     if (args.empty()) return ;
-
     std::string commandName = args[0];
-    Command* command = createCommand(commandName);
+    std::time_t end = std::time(NULL);
+    Command     *command = createCommand(commandName);
 
-    if (command) {
+    std::cout << "[DEBUG] what time is it : " << std::difftime(end, _clients[clientFd]->getConnectTime()) << std::endl;
+    if (command)
         executeCommand(command, clientFd, args);
-    } else {
+    // Vérifier si 200 secondes se sont écoulées depuis la connexion du client
+    // else if (std::difftime(end, _clients[clientFd]->getConnectTime()) >= 20) {
+    //     // Envoyer un PING pour vérifier la connexion
+    //     std::cout << "Envoyer un PING pour vérifier la connexion " << _clients[clientFd]->getConnectTime() << std::endl;
+    //     Command* ping = createCommand("PING");
+    //     if (ping) {
+    //         // Création du vecteur d'arguments avec la syntaxe C++98
+    //         std::vector<std::string> pingArgs;
+    //         pingArgs.push_back("PING");
+    //         pingArgs.push_back("localhost");
+
+    //         executeCommand(ping, clientFd, pingArgs);
+    //     }
+    // }
+    // Si aucune commande valide, envoyer un message d'erreur
+    else
         _clients[clientFd]->messageSend("\033[31mUnknown command\r\n\033[0m");
-    }
 }
+
+
 
 void CommandHandler::executeCommand(Command* command, int clientFd, const std::vector<std::string>& args) {
     command->execute(clientFd, _clients, args);
